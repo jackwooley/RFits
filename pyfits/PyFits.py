75083dbbb9d8b188df4205afb07bd1100e48ff4a
jack <jack.wooley100@gmail.com>
2022-11-30 11:38:18 -0700

    updated the roadmap and added modify/create closet functions, moved some import statements to be global variables

--- pyfits/PyFits.py
+++ pyfits/PyFits.py
@@ -3,15 +3,34 @@ import random
 from re import split
 import json
 
+CLOTHING_TYPES = str(
+    input("""Enter a comma-separated list of the different clothing categories you'd like to include."""))
+MODIFY_WRITE_OR_IGNORE_PARAMETER = str(input("""Enter the letter w to create or overwrite your current closet, a to modify, \nor any other letter to skip this step if you are happy with what is currently in it."""))
 
-def category_keys():
+
+def main():
+    # fill up the closet with clothing types (keys)
+    closet_keys = category_keys(CLOTHING_TYPES)
+
+    # fill up the closet with clothing items (values)
+    full_closet = fill_my_closet(closet_keys, MODIFY_WRITE_OR_IGNORE_PARAMETER)
+
+    # change the mode string to lower and make sure it's an accepted input
+    which_mode_lower = MODIFY_WRITE_OR_IGNORE_PARAMETER.lower()
+    if ord(which_mode_lower) == 119:
+        write_closet_to_json(full_closet, which_mode_lower)
+    elif ord(which_mode_lower) == 97:
+        keys_to_modify = str(
+        input("""Which clothing categories would you like to update? Enter a comma-separated list."""))
+        list_of_keys_to_modify = split(",\s*", keys_to_modify)
+        modify_closet_json(list_of_keys_to_modify)
+
+
+def category_keys(clothing_type_inputs: str):
     """Create a dictionary with keys and no values. Keys correspond to
      different clothing categories.
     """
 
-    # type_list = list(str(input("""Enter a comma-separated list of the different clothing categories you'd like to include.""")).split(', '))
-
-    clothing_type_inputs = str(input("""Enter a comma-separated list of the different clothing categories you'd like to include."""))
     type_list = split(",\s*", clothing_type_inputs)
 
     clothes_dict = {}
@@ -20,10 +39,10 @@ def category_keys():
     return clothes_dict
 
 
-test_dict = category_keys()
+# test_dict = category_keys()
 
 
-def fill_my_closet(which_dict: dict):
+def fill_my_closet(which_dict: dict, mode: str):
     """Add the clothes values into the previously created dictionary.
 
     Keyword arguments:
@@ -37,14 +56,23 @@ def fill_my_closet(which_dict: dict):
         input_for_choosing = str(input(f"Enter a comma-separated list of clothing items in the {j} category."))
         which_dict[j] = split(",\s*", input_for_choosing)
 
-    # write the dict to a json so that you can
-    with open('closet.json', 'w') as outfile:
-        json.dump(which_dict, outfile)
+    write_closet_to_json(which_dict, mode)
 
     return which_dict
 
 
-fill_my_closet(test_dict)
+def write_closet_to_json(closet_dict: dict, which_mode: str):
+    # write the dict to a json so that you can save it and edit it later
+    with open('closet.json', which_mode) as outfile:
+        json.dump(closet_dict, outfile)
+
+
+def modify_closet_json(closet_dict: dict, *args):
+    # todo: add a section to open the json as a dictionary
+
+    # append the input to each category
+    for i in args:
+        closet_dict[i].append(split(",\s*", input(f"""What do you want to add to the {i} category? Enter a comma-separated list.""")))
 
 
 def outfit_selector_function(closet: dict):
@@ -60,6 +88,6 @@ def outfit_selector_function(closet: dict):
     return new_fit
 
 
-blind = outfit_selector_function(test_dict)
-
-print('for debugging')
+# blind = outfit_selector_function(test_dict)
+#
+# print('for debugging')
